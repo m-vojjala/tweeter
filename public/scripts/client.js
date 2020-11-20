bd2faa73d1a85840a8dea93d6713e9c2c3a1dd01
MounikaVojjala <mounikavojjala@gmail.com>
2020-11-20 02:38:46 +0000

    comments added

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,15 +7,15 @@
 $(document).ready(function () {
   $("#error-message").hide();
 
-  // $('form textarea')
+  // Creating structure of tweets dynamically
   const createTweetElement = SingleTweet => {
-    const escape =  function(str) {
+    const escape = function (str) {
       let div = document.createElement('div');
       div.appendChild(document.createTextNode(str));
       return div.innerHTML;
     }
-     console.log(SingleTweet)
-    // const safeHTML = `<p>${escape(SingleTweet.content.text)}</p>`;
+    const unixTimestamp = Date(SingleTweet.created_at);
+    const milliseconds = unixTimestamp.toString();
     const $tweet =
       $(`<article class="tweet">
     <header>
@@ -27,7 +27,7 @@ $(document).ready(function () {
     </header>
     <p>${escape(SingleTweet.content.text)}</p>
     <footer>
-    <span><strong>${SingleTweet.created_at}</strong></span>
+    <span><strong>${milliseconds}</strong></span>
     <div>
       <i class="fas blue fa-flag"></i>
       <i class="fas blue fa-sync"></i>
@@ -36,69 +36,67 @@ $(document).ready(function () {
     </div>
   </footer>
   </article>`)
-
     return $tweet;
   }
- 
 
+  // Takes the array of tweets and looped through all the tweets and added to the section(tweets-container)
   const renderTweets = (allTweets) => {
-    //  console.log(All)
     for (let tweet of allTweets) {
-      //  console.log(`tweet ${tweet}`);
       const $newTweet = createTweetElement(tweet);
-      // console.log(`newtweet:${$newTweet}`)
       $("#tweets-container").prepend($newTweet);
     }
   }
-  
 
- $('.tweet-button').on('click',event=>{
-   $(".compose-tweet").toggle("slow");
- });
+  // function to make "write a tweet" button work when clicked
+  $('.tweet-button').on('click', event => {
+    event.preventDefault();
+    $(".compose-tweet").toggle("slow");
+  });
 
+  // on submit of form validation is done to check the input is valid or not 
+  // posts the tweets to the server using AJAX
+  $('form').on("submit", event => {
+    event.preventDefault();
+    const tweet = $('#tweet-text').val();
+    if (tweet === "" || tweet === null) {
+      $("#error-message").text("Invalid Tweet!!")
+      $("#error-message").slideDown("slow");
+      setTimeout(function () {
+        $("#error-message").slideUp("slow");
+      }, 3000)
+    } else if (tweet.length > 140) {
+      $("#error-message").text("Tweet must not exceed 140 characters!!")
+      $("#error-message").slideDown("slow");
+      setTimeout(function () {
+        $("#error-message").slideUp("slow");
+      }, 3000)
+    } else {
+      $
+        .ajax({
+          url: "/tweets",
+          method: "POST",
+          data: $('form').serialize()
+        }).then(() => {
+          $("#tweets-container").empty();
+          $("#error-message").hide("slow");
+          loadTweets();
+        });
+    }
+  });
 
-$('form').on("submit",event=>{
-event.preventDefault();
-const tweet = $('#tweet-text').val();
-if(tweet === "" || tweet === null){
-  $("#error-message").text("Invalid Tweet!!")
- $("#error-message").slideDown("slow")
- setTimeout(function(){
-  $("#error-message").slideUp("slow");
-}, 3000)
-}else if(tweet.length > 140){
-  $("#error-message").text("Tweet must not exceed 140 characters!!")
-  $("#error-message").slideDown("slow")
-  setTimeout(function(){
-    $("#error-message").slideUp("slow");
-  }, 3000)
-}else{
-$
- .ajax({
-   url:"/tweets",
-   method:"POST",
-   data:$('form').serialize()
- }).then(()=>{
-  $("#tweets-container").empty();
-  $("#error-message").hide("slow");
-loadTweets();
- });
-}
+  // Fetched the tweets using AJAX
+  const loadTweets = () => {
+    $
+      .ajax("/tweets", { method: 'GET', dataType: "json" })
+      .then(function (res) {
+        //  console.log(res);
+        renderTweets(res);
+      });
+  }
+  loadTweets();
 });
 
-const loadTweets = () =>{
-  $
-   .ajax("/tweets",{method: 'GET',dataType: "json"})
-   .then(function(res){
-    //  console.log(res);
-      renderTweets(res)
-    });
-}
-loadTweets();
-});
 
 
 
 
-
- 
